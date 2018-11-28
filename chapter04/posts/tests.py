fdb7d44da080a0c0aace558aa0b0fa1081f7881c
Ezio <assassinste@gmail.com>
2018-11-28 13:57:47 +0800

    Test::p2 - 'Is it the correct urls & templates ?'

--- chapter04/posts/tests.py
+++ chapter04/posts/tests.py
@@ -1,13 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
 from .models import Post
 
 
 class PostModelTest(TestCase):
 
+    # It's just for enabling us to run subseq tests 
     def setUp(self):
-        Post.objects.create(text="just a test post")
+        Post.objects.create(text="a simple test")
 
+    # any func starts with 'test*' and in the 'tests.py'
+    #   will be tested while running the 'python manage.py test'
     def test_text_content(self):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
-        self.assertEqual(expected_object_name, 'just a test post')
+        self.assertEqual(expected_object_name, 'a simple test')
+
+
+class HomePageView(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='yet another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
