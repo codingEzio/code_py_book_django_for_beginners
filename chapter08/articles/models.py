c687fe5d555bdb47c367108115f12ea8d4a6bff0
Ezio <assassinste@gmail.com>
2018-12-04 22:13:17 +0800

    Now the articles' page could display its comments (view-only, cannot-edit)

--- chapter08/articles/models.py
+++ chapter08/articles/models.py
@@ -18,9 +18,19 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
     comment = models.CharField(max_length=140)
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.comment
